830d19c3827c10d58c1ef0707a477b1f453b3d3b
RCYGM <jurgenmuller997@gmail.com>
2025-01-14 15:54:52 +0100

    feat: feat/file-metadata-microservice v1

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Carpeta donde se guardarán los archivos
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); // Nombre único
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const fileDate = req.file;
+  console.log("fileDate", fileDate);
+  res.json({
+    name: fileDate.originalname,
+    type: fileDate.mimetype,
+    size: fileDate.size,
+  });
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
